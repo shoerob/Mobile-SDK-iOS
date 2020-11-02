79aa88a8cb6db0184bd3405d04f3f46c0a5f7a8f
Robert Shoemate <shoerob@gmail.com>
2020-11-02 13:39:54 -0600

    added another LDM attempt.

--- Sample Code/SwiftSampleCode/DJISDKSwiftDemo/Services/ProductCommunicationManager.swift
+++ Sample Code/SwiftSampleCode/DJISDKSwiftDemo/Services/ProductCommunicationManager.swift
@@ -17,6 +17,9 @@ class ProductCommunicationManager: NSObject {
     // When enableBridgeMode is set to true, set this value to the IP of your bridge app.
     let bridgeAppIP = "10.81.55.116"
     
+    var attemptToEnableLDMAfterRegistrationSucceeds: Bool = false
+    var attemptToEnableLDMAfterProductConnects: Bool = false
+    
     func registerWithSDK() {
         let appKey = Bundle.main.object(forInfoDictionaryKey: SDK_APP_KEY_INFO_PLIST_KEY) as? String
         
@@ -32,7 +35,9 @@ class ProductCommunicationManager: NSObject {
         
 //        self.AttemptToUseLDM_1()
 //        self.AttemptToUseLDM_2()
-        self.AttemptToUseLDM_3()
+//        self.AttemptToUseLDM_3()
+//        self.AttemptToUseLDM_4()
+        self.AttemptToUseLDM_5()
     }
     
     // Attempt #1
@@ -50,7 +55,7 @@ class ProductCommunicationManager: NSObject {
     // Resulting Output:
     //   DEBUG -- getIsLDMSupported: false, The drone is not connect(code:-12000)
     //
-    // Conclusion(s): Does NOT work. The observer for DJILDMManagerSupportedChanged is NEVER called. registerApp NEVER occurs.
+    // Conclusion(s): FAILURE. Does NOT work. The observer for DJILDMManagerSupportedChanged is NEVER called. registerApp NEVER occurs.
     //
     // MARK: LDM Attempt #1
     func AttemptToUseLDM_1() {
@@ -91,7 +96,7 @@ class ProductCommunicationManager: NSObject {
     //   DEBUG -- getIsLDMSupported: false, The drone is not connect(code:-12000)
     //   DJILDMManagerSupportedChanged -- isSupported: false -- isEnabled: false
     //
-    // Conclusion(s): Does NOT work. isSupported is always FALSE.
+    // Conclusion(s): FAILURE. Does NOT work. isSupported is always FALSE.
     //
     func AttemptToUseLDM_2() {
         DJISDKManager.registerApp(with: self)
@@ -135,7 +140,7 @@ class ProductCommunicationManager: NSObject {
     //   DEBUG -- DJILDMManagerSupportedChanged -- isSupported: false -- isEnabled: false
     //   DEBUG -- getIsLDMSupported: false, The drone is not connect(code:-12000)
     //
-    // Conclusion(s): Does NOT work. isSupported is always FALSE.
+    // Conclusion(s): FAILURE. Does NOT work. isSupported is always FALSE.
     //
     func AttemptToUseLDM_3() {
         DJISDKManager.registerApp(with: self)
@@ -170,6 +175,143 @@ class ProductCommunicationManager: NSObject {
             }
         }
     }
+    
+    // MARK: LDM Attempt #4
+    //   This code registers the app with DJI before attempting to enable LDM. Once app registration succeeds,
+    //   getIsLDMSupported is called from the appRegisteredWithError delegate callback method.
+    //
+    //
+    // Resulting Output:
+    //   2020-11-02 13:07:38.562171-0600 DJISDKSwiftDemo[2135:817559] SDK Registered with error nil
+    //   DEBUG -- getIsLDMSupported: false, The drone is not connect(code:-12000)
+    //   DEBUG -- DJILDMManagerSupportedChanged -- isSupported: false -- isEnabled: false
+    //   DEBUG -- DJILDMManagerSupportedChanged -- isSupported: false -- isEnabled: false
+    //
+    // Conclusion(s): FAILURE. Does NOT work. isSupported is always FALSE.
+    //
+    func AttemptToUseLDM_4() {
+        self.attemptToEnableLDMAfterRegistrationSucceeds = true
+        
+        DJISDKManager.registerApp(with: self)
+        
+        NotificationCenter.default.addObserver(forName: NSNotification.Name.DJILDMManagerSupportedChanged, object: nil, queue: nil) { (notification) in
+            let isSupported = DJISDKManager.ldmManager().isLDMSupported
+            let isEnabled = DJISDKManager.ldmManager().isLDMEnabled
+            print("DEBUG -- DJILDMManagerSupportedChanged -- isSupported: \(isSupported) -- isEnabled: \(isEnabled)")
+            
+            if isSupported == true {
+                if !DJISDKManager.hasSDKRegistered() {
+                    DJISDKManager.registerApp(with: self)
+                }
+                
+                if !DJISDKManager.ldmManager().isLDMEnabled {
+                    DJISDKManager.ldmManager().enableLDM { (error) in
+                        print("DEBUG -- enableLDM Result: \(error?.localizedDescription ?? "nil")")
+                    }
+                }
+            }
+        }
+        
+        NotificationCenter.default.addObserver(forName: NSNotification.Name.DJILDMManagerEnabledChanged, object: nil, queue: nil) { (notification) in
+            let isSupported = DJISDKManager.ldmManager().isLDMSupported
+            let isEnabled = DJISDKManager.ldmManager().isLDMEnabled
+            print("DEBUG -- DJILDMManagerEnabledChanged -- isSupported: \(isSupported) -- isEnabled: \(isEnabled)")
+        }
+    }
+    
+    // MARK: LDM Attempt #5
+    //   This code registers the app with DJI before attempting to enable LDM. Once app registration succeeds,
+    //   getIsLDMSupported is called from the productConnected delegate callback method.
+    //
+    //
+    // Resulting Output:
+    //
+    //   Run #1 - Install application FROM SCRATCH. Start application. Plug in DJI controller to iPad after started. (SUCCESS)
+    //
+    //   DEBUG -- DJILDMManagerSupportedChanged -- isSupported: false -- isEnabled: false
+    //   2020-11-02 13:21:41.453384-0600 DJISDKSwiftDemo[2187:824957] SDK Registered with error nil
+    //   ----- NOTE: Plugged in Controller Here -----
+    //   DEBUG -- getIsLDMSupported: true, nil
+    //   DEBUG -- DJILDMManagerSupportedChanged -- isSupported: true -- isEnabled: false
+    //   DEBUG -- enableLDM Result: nil
+    //   DEBUG -- DJILDMManagerEnabledChanged -- isSupported: true -- isEnabled: true
+    //
+    // Conclusion(s): Works.
+    //
+    //   Run #2a - Re-start the ALREADY INSTALLED application. Plug in DJI controller to iPad after started. (FAILURE)
+    //
+    //   2020-11-02 13:25:08.305945-0600 DJISDKSwiftDemo[2196:826257] SDK Registered with error nil
+    //   DEBUG -- DJILDMManagerSupportedChanged -- isSupported: true -- isEnabled: false
+    //   DEBUG -- enableLDM Result: The drone is not connect(code:-12000)
+    //   DEBUG -- DJILDMManagerSupportedChanged -- isSupported: true -- isEnabled: false
+    //   DEBUG -- enableLDM Result: The drone is not connect(code:-12000)
+    //   ----- NOTE: Plugged in Controller Here -----
+    //   DEBUG -- getIsLDMSupported: true, nil
+    //   DEBUG -- DJILDMManagerSupportedChanged -- isSupported: true -- isEnabled: false
+    //   DEBUG -- enableLDM Result: nil
+    //   DEBUG -- DJILDMManagerEnabledChanged -- isSupported: true -- isEnabled: true
+    //
+    //   Run #2b - Re-start the ALREADY INSTALLED application. Plug in DJI controller to iPad after started.
+    //
+    //   2020-11-02 13:27:36.105117-0600 DJISDKSwiftDemo[2205:827358] SDK Registered with error nil
+    //   DEBUG -- DJILDMManagerSupportedChanged -- isSupported: true -- isEnabled: false
+    //   DEBUG -- enableLDM Result: The drone is not connect(code:-12000)
+    //   ----- NOTE: Plugged in Controller Here -----
+    //   DEBUG -- getIsLDMSupported: false, The drone is not connect(code:-12000)
+    //
+    //   Run #2c - Re-start the ALREADY INSTALLED application. Plug in DJI controller to iPad after started. (SUCCESS)
+    //
+    //   2020-11-02 13:30:22.534995-0600 DJISDKSwiftDemo[2214:828483] SDK Registered with error nil
+    //   DEBUG -- DJILDMManagerSupportedChanged -- isSupported: true -- isEnabled: false
+    //   DEBUG -- enableLDM Result: The drone is not connect(code:-12000)
+    //   DEBUG -- DJILDMManagerSupportedChanged -- isSupported: true -- isEnabled: false
+    //   DEBUG -- enableLDM Result: The drone is not connect(code:-12000)
+    //   ----- NOTE: Plugged in Controller Here -----
+    //   DEBUG -- getIsLDMSupported: true, nil
+    //   DEBUG -- DJILDMManagerSupportedChanged -- isSupported: true -- isEnabled: false
+    //   DEBUG -- enableLDM Result: nil
+    //   DEBUG -- DJILDMManagerEnabledChanged -- isSupported: true -- isEnabled: true
+    //
+    //   Run #3 - Plugin the DJI controller BEFORE starting the app. Re-start the ALREADY INSTALLED application. (FAILURE)
+    //
+    //   2020-11-02 13:34:59.766597-0600 DJISDKSwiftDemo[2223:829916] SDK Registered with error nil
+    //   DEBUG -- DJILDMManagerSupportedChanged -- isSupported: true -- isEnabled: false
+    //   DEBUG -- enableLDM Result: The drone is not connect(code:-12000)
+    //   DEBUG -- getIsLDMSupported: false, The drone is not connect(code:-12000)
+    //   DEBUG -- DJILDMManagerSupportedChanged -- isSupported: true -- isEnabled: false
+    //   DEBUG -- enableLDM Result: The drone is not connect(code:-12000)
+    //
+    //   Conclusion(s): FAILURE. Works sometimes. Does NOT work other times.
+    //
+    func AttemptToUseLDM_5() {
+        self.attemptToEnableLDMAfterProductConnects = true
+        
+        DJISDKManager.registerApp(with: self)
+        
+        NotificationCenter.default.addObserver(forName: NSNotification.Name.DJILDMManagerSupportedChanged, object: nil, queue: nil) { (notification) in
+            let isSupported = DJISDKManager.ldmManager().isLDMSupported
+            let isEnabled = DJISDKManager.ldmManager().isLDMEnabled
+            print("DEBUG -- DJILDMManagerSupportedChanged -- isSupported: \(isSupported) -- isEnabled: \(isEnabled)")
+            
+            if isSupported == true {
+                if !DJISDKManager.hasSDKRegistered() {
+                    DJISDKManager.registerApp(with: self)
+                }
+                
+                if !DJISDKManager.ldmManager().isLDMEnabled {
+                    DJISDKManager.ldmManager().enableLDM { (error) in
+                        print("DEBUG -- enableLDM Result: \(error?.localizedDescription ?? "nil")")
+                    }
+                }
+            }
+        }
+        
+        NotificationCenter.default.addObserver(forName: NSNotification.Name.DJILDMManagerEnabledChanged, object: nil, queue: nil) { (notification) in
+            let isSupported = DJISDKManager.ldmManager().isLDMSupported
+            let isEnabled = DJISDKManager.ldmManager().isLDMEnabled
+            print("DEBUG -- DJILDMManagerEnabledChanged -- isSupported: \(isSupported) -- isEnabled: \(isEnabled)")
+        }
+    }
 }
 
 extension ProductCommunicationManager : DJISDKManagerDelegate {
@@ -187,10 +329,19 @@ extension ProductCommunicationManager : DJISDKManagerDelegate {
             DJISDKManager.startConnectionToProduct()
         }
         
+        if self.attemptToEnableLDMAfterRegistrationSucceeds {
+            DJISDKManager.ldmManager().getIsLDMSupported { (isSupported, error) in
+                print("DEBUG -- getIsLDMSupported: \(isSupported), \(error?.localizedDescription ?? "nil")")
+            }
+        }
     }
     
     func productConnected(_ product: DJIBaseProduct?) {
-        
+        if self.attemptToEnableLDMAfterProductConnects {
+            DJISDKManager.ldmManager().getIsLDMSupported { (isSupported, error) in
+                print("DEBUG -- getIsLDMSupported: \(isSupported), \(error?.localizedDescription ?? "nil")")
+            }
+        }
     }
     
     func productDisconnected() {
